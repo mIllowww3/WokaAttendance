da4bfcc9e5aa9f38c12ef5afb50918c1f2917dab
mIllowww3 <vivianpratama381@gmail.com>
2025-11-24 12:04:40 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Dashboard</span>
+            <span class="nav-link-text ms-1">Dashboards</span>
           </a>
         </li>
         <li class="nav-item">
