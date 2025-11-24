d8915d3fedfdab9e66f025324a29bf1aec0851ab
billyaaandika <billyandikapradana@gmail.com>
2025-11-24 11:47:58 +0700

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,6 +52,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect()->route('logins');
     }
 }
