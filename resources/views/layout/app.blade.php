43608defd42dd44e453e13db32be4477915d574d
mIllowww3 <vivianpratama381@gmail.com>
2025-11-26 09:31:26 +0700

    tes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -66,7 +66,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('jadwal.index') }}">
+                    <a class="nav-link" href="{{ route('admin.jadwal.index') }}">
                         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="ni ni-app text-dark text-sm opacity-10"></i>
                         </div>
