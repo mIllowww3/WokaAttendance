9d25e866ad9b35f65744eeef0f1fc4b495f32c2a
billyaaandika <billyandikapradana@gmail.com>
2025-12-02 13:31:27 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.app')
 
 @section('content')
+
+@section('title', 'Dashboard')
+
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
